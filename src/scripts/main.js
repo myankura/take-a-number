9756ee3a124bf0826057938c2c8218339f851f48
Michael Yankura <myankura210@gmail.com>
2019-02-28 14:01:24 -0600

    Push completed Take-A-Number to repo

---
+++ src/scripts/main.js
@@ -0,0 +1,27 @@
+// A local nightclub is having a Battle of the Bands night in a few months. 
+// They expect many bands to sign up because the grand prize is $20,000. 
+// To make things easier on the nightclub management, they want each band 
+// to be assigned a number so that they can easily keep track of the order 
+// in which the bands will perform.
+
+// Your job is to write a function that accepts any band name as an argument. 
+// The function will increment a global variable by one each time it is invoked, 
+// and return that number, and the band name concatenated together.
+
+let bandNumber = 1;
+
+const takeNumber = function (bandName) {
+    //create a variable to store band number to later be incremented
+    let num = bandNumber;
+    //increment bandNumber by one each time
+    bandNumber++;
+    //return the order in which the band will play and their name
+    return `${num} ${bandName}`;
+}
+
+//This is where all of the bandNames will be entered.
+const scum = takeNumber("Galactic Scum");
+console.log(scum); // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs");
+console.log(under); // This should print "2. Underdogs" in the console
